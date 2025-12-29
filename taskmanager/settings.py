c51a1395edf5e3e0c242db8ba6621210484e086c
aditya2081 <22A81A6102@sves.org.in>
2025-12-29 19:17:24 +0530

    Fix: Add .onrender.com to ALLOWED_HOSTS

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-chang
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.railway.app,.up.railway.app', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,.railway.app,.up.railway.app,.onrender.com', cast=Csv())
 
 
 # Application definition
